b4e1695bac62bf58956f18dff2f3959fe8fb3cb6
samdial69 <sdiallo585@gmail.com>
2021-11-24 14:31:49 +0100

    Initializing and configuring application : connexion to database done here

---
+++ src/main/java/fr/univlorrainem1archi/friendsfiestas_v1/FriendsFiestasV1Application.java
@@ -0,0 +1,13 @@
+package fr.univlorrainem1archi.friendsfiestas_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendsFiestasV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendsFiestasV1Application.class, args);
+	}
+
+}
